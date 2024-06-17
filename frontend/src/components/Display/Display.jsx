8531ce0d07cda7fa6d2db1db85a8453bff7fc259
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-06-17 15:37:21 +0530

    Top Plants
    all errors fixed
    assetes, item added

--- frontend/src/components/Display/Display.jsx
+++ frontend/src/components/Display/Display.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import './Display.css'
-import { StoreContext } from '../context/StoreContext'
+import React, { useContext } from 'react';
+import './Display.css';
+import { StoreContext } from '../context/StoreContext';
+import Item from '../Item/Item';
 
-const Display = ({category}) => {
+const Display = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
 
-    const {food_list} = useContext(StoreContext)
   return (
     <div className='display' id='display'>
-        <h2>Top Plants</h2>  
-
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-                return
-            })}
-        </div>      
-   </div>
-  )
+      <h2>Top Plants</h2>
+      <div className="food-display-list">
+        {food_list
+          .filter(item => category === "All" || item.category === category) // Optional: filter items based on category
+          .map((item, index) => (
+            <Item
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import { food_list } from "../../assets/assets";
 export const StoreContext = createContext(null)
 
+
 const StoreContextProvider = (props) => {
 
 
