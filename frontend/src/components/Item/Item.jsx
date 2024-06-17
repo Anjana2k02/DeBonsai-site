495f77fa8aa58c5e2aa386cd5e2b39252b1166be
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-06-17 16:18:23 +0530

    create all menu options
    done

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import { assets } from '../../assets/assets';
+import './item.css';
 
-
-const Item = ({id,name,price,description,image}) => {
+const Item = ({ id, name, price, description, image }) => {
   return (
     <div className='food-item'>
-    <div className="food-item-img-container">
-        <img className='food-item.img' src={image} alt=""/>
+      <div className="food-item-image-container">
+        <img className='food-item-image' src={image} alt={name} />
+      </div>
+      <div className='food-item-info'>
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt=""/>
         </div>
-        <div className='food-item-info'>
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt=""/>
-
-            </div>
-            <p className="food-item-desc">{description}</p>
-            <p className="food-item-price">${price}</p>
-        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
