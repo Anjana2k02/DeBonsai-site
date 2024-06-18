310c13aa3a9c3e6db878c2580128d2c4c34f2399
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-06-18 12:54:25 +0530

    footer
    added footer, fixed some errors previous

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
+    <>
     <div className="app">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      
     </div>
+    <Footer />
+    </>
+    
   );
 };
 
--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -7,6 +7,7 @@ const Item = ({ id, name, price, description, image }) => {
     <div className='food-item'>
       <div className="food-item-image-container">
         <img className='food-item-image' src={image} alt={name} />
+        
       </div>
       <div className='food-item-info'>
         <div className="food-item-name-rating">
